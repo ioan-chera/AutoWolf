27c359473f7329c4a2caa843b90466cb14edcadc
printz <printz@ed9619e2-446b-452b-b3fd-19d70bdbfe38>
2012-12-23 10:49:33 +0000

    Started the dictionary part, which will be used by the bot.
    git-svn-id: svn+ssh://svn.code.sf.net/p/autowolf/code-0/trunk@67 ed9619e2-446b-452b-b3fd-19d70bdbfe38

---
+++ Dictionary.cpp
@@ -0,0 +1,9 @@
+//
+//  Dictionary.cpp
+//  Wolf4SDL
+//
+//  Created by Ioan on 21.12.2012.
+//
+//
+
+#include "Dictionary.h"
---
+++ Dictionary.h
@@ -0,0 +1,25 @@
+//
+//  Dictionary.h
+//  Wolf4SDL
+//
+//  Created by Ioan on 21.12.2012.
+//
+//
+
+#ifndef DICTIONARY_H_
+#define DICTIONARY_H_
+
+#include "List.h"
+
+//
+// Dictionary
+//
+// A list of string keys with variant values
+//
+class Dictionary
+{
+	
+};
+
+#endif
+
