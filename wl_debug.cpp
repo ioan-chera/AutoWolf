099cdfdcbaa3c397defa53730c0afe96de5fcb72
ioan <ioan_chera@yahoo.com>
2013-02-02 23:22:22 +0200

    SPEAR stage 10: wl_debug unification

--- wl_debug.cpp
+++ wl_debug.cpp
@@ -670,21 +670,19 @@ again:
     {
         CenterWindow(26,3);
         PrintY+=6;
-#ifndef SPEAR
-        US_Print("  Warp to which level(1-10): ");
-#else
-        US_Print("  Warp to which level(1-21): ");
-#endif
+        // IOANCH 20130202: unification process
+        if(!SPEAR)
+            US_Print("  Warp to which level(1-10): ");
+        else
+            US_Print("  Warp to which level(1-21): ");
+
         VW_UpdateScreen();
         esc = !US_LineInput (px,py,str,NULL,true,2,0);
         if (!esc)
         {
             level = atoi (str);
-#ifndef SPEAR
-            if (level>0 && level<11)
-#else
-            if (level>0 && level<22)
-#endif
+            // IOANCH 20130202: unification process
+            if (!SPEAR && level>0 && level<11 || SPEAR && level > 0 && level < 22)
             {
                 gamestate.mapon = level-1;
                 playstate = ex_warped;
