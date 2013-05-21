922ab2dae58f9aad49d202140d948cf11c56ad84
Ioan Chera <ioan_chera@yahoo.com>
2013-05-21 13:39:33 +0300

    Fixed some command-line compilation requirements.

--- src/Config.cpp
+++ src/Config.cpp
@@ -26,7 +26,7 @@
 
 #include <sys/stat.h>
 #ifdef __APPLE__
-#include "CocoaFun.h"
+#include "macosx/CocoaFun.h"
 #endif
 #include "Config.h"
 #include "PString.h"
@@ -337,7 +337,7 @@ void Config::CheckParameters(int argc, char *argv[])
         puts(e.info().buffer());
         printf(
                "AutoWolf v0.3\n"
-               "By Ioan Chera on Wolf4SDL codebase"
+               "By Ioan Chera on Wolf4SDL codebase\n"
                "Wolf4SDL: Ported by Chaos-Software (http://www.chaos-software.de.vu)\n"
                "Original Wolfenstein 3D by id Software\n\n"
 #ifdef __APPLE__
